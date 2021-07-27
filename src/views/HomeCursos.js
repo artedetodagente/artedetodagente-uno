1733ae41f82e8057fac03c112a487a1517e27834
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2021-07-27 17:28:58 -0300

    Modificaçao da descriçao do curso

--- src/views/HomeCursos.js
+++ src/views/HomeCursos.js
@@ -2,27 +2,25 @@ import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
 
 function HomeCursos(props) {
-  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSdXVaN3qYt78FPGj_5334VGXHz4kKicZctgRm7xkwWSQVHkIA/viewform`
+  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSeHHfAvFVgqc82Yahv7OVnOIbS8UuJjK789f2c8EYsL33_Ypw/viewform`
 
   return (
     <section id="cursos" className="base cursos">
       <div className="title-box divider">
         <h2>
           <Link to="/#cursos">
-            Curso Música + Educação + Acessibilidade
+            Dança + Educação + Acessibilidade
           </Link>
         </h2>
       </div>
       <div className="inside-content">
         <div>
           <p>
-            Estão abertas até o dia 12/7, aqui no site, as inscrições para a segunda turma do curso Música + Educação + Acessibilidade, do projeto Um Novo Olhar. A coordenação de conteúdo é da professora Thelma Alvares, que divide as aulas, assíncronas, com os professores  Viviane Louro e Fernando Guilhon. O curso será ministrado no Ambiente Virtual de Aprendizagem da UFRJ. Haverá emissão de certificado, com carga horária de 48 horas, para os cursistas que obtiverem aprovação por meio de uma avaliação sobre o conteúdo apresentado. Saiba mais sobre o curso aqui:
+            Estão abertas aqui no site, até 9/8, as inscrições para o curso Dança + Educação + Acessibilidade, destinado a professores do ensino fundamental e demais profissionais interessados em arte/educação e acessibilidade. Na pauta, temas que envolvem as relações entre os campos da dança, da dança-educação e da acessibilidade cultural, para fornecer ao professor maior capacitação para trabalhar com elementos da dança na diversidade. O curso é gratuito, terá aulas assíncronas e será realizado em ambiente virtual, com início em 16/8 e conclusão até 10/10. Estão disponíveis 600 vagas para todo o país.
           </p>
           <p>
-            O curso foi desenvolvido por professores de música, mas não tem como proposta a educação musical. Ele foi estruturado para professores que não são músicos – a partir do entendimento de que a música pode contribuir com o aprimoramento do ensino regular. 
-          </p>
-          <p>
-            Na série, será discutida a relação profunda do homem com a música e, ainda, de que modo essa forma de expressão artística influencia no desenvolvimento humano. Os vídeos abordam temas como: o trabalho direcionado a pessoas deficiência; a música relacionada à inclusão desses cidadãos; as adaptações que facilitam o desenvolvimento dessa atividade; e o trabalho musical direcionado a pessoas com diferentes deficiências: intelectual, visual, auditiva, surdez e transtorno do espectro autista.
+            A atividade faz parte da série de Arte/Educação + Acessibilidade + Inclusão do projeto Um Novo Olhar, parceria da Fundação Nacional de Artes – Funarte com a Universidade Federal do Rio de Janeiro – UFRJ.
+            Link inscrições:
           </p>
           <a 
             href={formUrl} 
@@ -34,7 +32,7 @@ function HomeCursos(props) {
           </a>
           
           &nbsp;
-          <Link className="link-box" to="/noticias/Abertas-as-inscricoes-para-a-2a-turma-do-curso-Musica-+-Educacao-+-Acessibilidade">
+          <Link className="link-box" to="/noticias/Estao-abertas-(ate-98)-as-inscricoes-para-o-curso-Danca-+-Educacao-+-Acessibilidade">
             Saiba mais
           </Link>
         </div>
