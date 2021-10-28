a83a0451fdec8e4d38ea3cdd066019c7ecf0c9ef
rlima-ferreira <renanferreiradelima98@live.com>
2021-10-28 16:03:00 -0300

    Inclusao do novo curso

--- src/views/HomeCimuci.js
+++ src/views/HomeCimuci.js
@@ -2,24 +2,21 @@ import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
 
 function HomeCimuci(props) {
-  const formUrl = `https://www.e-inscricao.com/cimuci/2021`
+  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSeV25tK3ro1xvMYaREN0QRljsJiSIMa9mXXNiRHfpmq4owoVw/viewform`
 
   return (
     <section id="cimuci" className="base cursos">
       <div className="title-box divider">
         <h2>
           <Link to="/#cimuci">
-            II CIMUCI - CONGRESSO INTERNACIONAL DE MÚSICA CORAL INFANTOJUVENIL – UM NOVO OLHAR
+            Curso Dança + Educação + Acessibilidade
           </Link>
         </h2>
       </div>
       <div className="inside-content">
         <div>
           <p>
-            O Projeto Um Novo Olhar, realizado pela Universidade Federal do Rio de Janeiro – UFRJ e a Fundação Nacional das Artes (FUNARTE), o Governo do Estado do Espírito Santo, Faculdade de Música do Espírito Santo (FAMES), a Universidade Federal do Mato Grosso do Sul (UFMS), e o Lar Donato Flores, de Tatuí, São Paulo, em parceria com a Nova Associação Brasileira de Regentes Corais (ABRACO) e a Asociacion de Directores de Coro de la Republica Argentina (ADICORA) apresentam o II CIMUCI - CONGRESSO INTERNACIONAL DE MÚSICA CORAL INFANTOJUVENIL – Um Novo olhar, o qual será realizado em formato híbrido, nos dias 14, 15, 16 e 17 de outubro de 2021, transmitido pelo canal do YouTube Arte de Toda Gente.
-          </p>
-          <p>
-            O Evento tem como principais objetivos, incentivar as reflexões sobre os caminhos plurais da atividade coral, voltada para o público infantojuvenil; dar a oportunidade de divulgação de trabalhos e pesquisas que vem sendo desenvolvidas; ampliar o intercâmbio entre as diferentes instituições que desenvolvem trabalhos voltados para esse público, em âmbito nacional e internacional. Nesta segunda edição estão previstas palestras, mesas redondas, oficinas e workshop, que contarão com a presença de destacados profissionais da música coral nacional e internacional, além de rica troca de experiências e saberes a partir das apresentações de trabalhos científicos, resultantes de pesquisas concluídas ou em andamento e de relatos de experiências resultante de atuação de professores, regentes, preparadores vocais, compositores e arranjadores para coros infantojuvenis.
+          O curso Dança + Educação + Acessibilidade aborda as relações entre o campo da dança, da dança-educação e da acessibilidade cultural e discute tópicos como as linguagens artísticas e as forças históricas e políticas que acompanharam esta arte em sua trajetória no ocidente, incluindo uma apresentação e experimentações relacionadas ao tema. A intenção é fomentar reflexões e fornecer ao professor maior capacitação para trabalhar com elementos da dança na diversidade. As inscrições são gratuitas e estarão abertas aqui no site de 28/10 a 10/11 próximos. O curso será realizado no período de 16/11 a 16 de janeiro de 2022.
           </p>
           <a 
             href={formUrl} 
