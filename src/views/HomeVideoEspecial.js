737c432c51f7e767b1896a5753ab47cc3697ddc7
Renan Ferreira de Lima <renan@Renan-PC.Renan-PC>
2022-03-25 09:27:53 -0300

    Redução do tamanho do vídeo em home

--- src/views/HomeVideoEspecial.js
+++ src/views/HomeVideoEspecial.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
-function HomeVideoEspecial(){
+function HomeVideoEspecial() {
 
-    const url = "https://youtu.be/jB0rFfC-G8k"
-    const vid = url.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=))([\w-]{10,12})\b/)[1]
+  const url = "https://youtu.be/jB0rFfC-G8k"
+  const vid = url.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=))([\w-]{10,12})\b/)[1]
 
-    return(
-        <>
-            <section className="base video-especial" style={{backgroundColor: '#fdf204'}}>
-                <div className="title-box divider">
-                    <h2>Cure o Mundo - Uma mensagem de esperança e cura</h2>
-                </div>
-                <div className="embed-container">
-                    <iframe
-                        title={vid}
-                        src={`https://www.youtube.com/embed/${vid}`}
-                        frameBorder="0"
-                        allow="fullscreen" 
-                    ></iframe>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="base video-especial" style={{ backgroundColor: '#fdf204' }}>
+        <div className="title-box divider">
+          <h2>Cure o Mundo - Uma mensagem de esperança e cura</h2>
+        </div>
+        <div className="embed-container" style={{ paddingBottom: 0, height: '170px' }}>
+          <iframe
+            title={vid}
+            src={`https://www.youtube.com/embed/${vid}`}
+            frameBorder="0"
+            allow="fullscreen"
+          ></iframe>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default HomeVideoEspecial;
